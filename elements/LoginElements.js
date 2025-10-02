1d6485a27d9f7ce4278f59453366d5056186a4ff
Marcos Maia <bateramarcomai@hotmail.com>
2025-10-02 11:31:13 -0300

    enviando o arquivo com os elementos

---
+++ elements/LoginElements.js
@@ -0,0 +1,15 @@
+module.exports = {
+  firstNameInput: 'input[name="first_name"]',
+  lastNameInput: 'input[name="last_name"]',
+  businessNameInput: 'input[name="business_name"]',
+  countrySelect: 'select[name="country_id"]',
+  whatsappInput: 'input[name="whatsapp"]',
+  emailInput: 'input[name="email"]',
+  submitButton: 'button[type="submit"]',
+  frontendUrlBox: '#demo .col-md-3:nth-child(1) a',
+  adminUrlBox: '#demo .col-md-3:nth-child(2) a',
+  usernameBox: '#demo .col-md-3:nth-child(3)',
+  passwordBox: '#demo .col-md-3:nth-child(4)'
+};
+
+
